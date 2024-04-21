3a761c14bbea1a1c360c075b515073bcc6e80241
rutkin <r.utkin91@gmail.com>
2024-04-22 00:02:23 +0300

    create extension

--- internal/repository/database.go
+++ internal/repository/database.go
@@ -29,6 +29,12 @@ func NewDatabase(databaseURI string) (*Database, error) {
 
 	defer tx.Rollback()
 
+	_, err = tx.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
+	if err != nil {
+		logger.Log.Error("Failed to create extension", zap.String("error", err.Error()))
+		return nil, err
+	}
+
 	_, err = tx.Exec("CREATE TABLE IF NOT EXISTS users (userID uuid DEFAULT gen_random_uuid(), userName VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (100) NOT NULL)")
 	if err != nil {
 		logger.Log.Error("Failed to create users table", zap.String("error", err.Error()))
