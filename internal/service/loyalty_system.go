743d577e4589689f20c65cef82af6457925ad212
rutkin <r.utkin91@gmail.com>
2024-04-25 01:07:57 +0300

    fix

--- internal/service/loyalty_system.go
+++ internal/service/loyalty_system.go
@@ -3,11 +3,14 @@ package service
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/rutkin/gofermart/internal/logger"
 	"github.com/rutkin/gofermart/internal/models"
 	"go.uber.org/zap"
 )
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
 
 func NewLoyaltySystem(address string) *LoyaltySystem {
 	return &LoyaltySystem{address}
@@ -18,10 +21,11 @@ type LoyaltySystem struct {
 }
 
 func (ls *LoyaltySystem) GetOrdersInfo(orderNumber string) (models.LoyaltyOrderRecord, error) {
-	resp, err := http.Get(ls.address + "api/users/" + orderNumber)
+	resp, err := myClient.Get(ls.address + "api/users/" + orderNumber)
 	var loyaltyOrder models.LoyaltyOrderRecord
 	if err := json.NewDecoder(resp.Body).Decode(&loyaltyOrder); err != nil {
 		logger.Log.Error("failed to decode loyalty order", zap.String("error", err.Error()))
 	}
+	defer resp.Body.Close()
 	return loyaltyOrder, err
 }
