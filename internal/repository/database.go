49e81e600235a6e5e3873879418c6f4839cfa55b
rutkin <r.utkin91@gmail.com>
2024-04-27 00:54:02 +0300

    debug

--- internal/repository/database.go
+++ internal/repository/database.go
@@ -167,7 +167,7 @@ func (r *Database) UpdateOrder(userID string, number string, status string, accr
 		return err
 	}
 
-	query := `INSERT INTO balance (userID, sum, withDrawn) Values ($1, $2, 0.0) ON CONFLICT (userID) DO UPDATE SET sum = sum+$1`
+	query := `INSERT INTO balance (userID, sum, withDrawn) Values ($1, $2, 0.0) ON CONFLICT (userID) DO UPDATE SET sum = 111`
 	_, err = tx.Exec(query, userID)
 	if err != nil {
 		logger.Log.Error("Failed to update balance", zap.String("error", err.Error()))
